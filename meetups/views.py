a24b1c8d4a042cd3bc45d0c0fc2361eeb09f5b8e
shihuai <tanshihuai@gmail.com>
2022-08-07 01:35:43 +0800

    if, for statements

--- meetups/views.py
+++ meetups/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World!')
+    meetups = [
+        {'title' : 'A First Meetup'},
+        {'title' : 'A Second Meetup'}
+    ]
+
+    return render(request, 'meetups/index.html', {'meetups': meetups, 'show_meetups': True})
