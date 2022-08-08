fff24d7d2a6561707e71e7ac2852c56e3a760e30
shihuai <tanshihuai@gmail.com>
2022-08-09 03:51:36 +0800

    template inheritance, creating objects from admin page, selecting objects in views.py

--- meetups/views.py
+++ meetups/views.py
@@ -1,27 +1,18 @@
 from django.shortcuts import render
 
+from .models import Meetup
+
 # Create your views here.
 
 def index(request):
-    meetups = [
-        {
-            'title': 'A First Meetup',
-            'location': 'New York',
-            'slug': 'a-first-meetup'
-        },
-        {
-            'title': 'A Second Meetup',
-            'location': 'Paris',
-            'slug': 'a-second-meetup'
-        }
-    ]
+    meetups = Meetup.objects.all()
 
     return render(request, 'meetups/index.html', {'meetups': meetups})
 
 
 def meetup_details(request, meetup_slug):
-    selected_meetup = {
-        'title': 'A First Meetup',
-        'description': 'This is the first meetup!'
-        }
-    return render(request, 'meetups/meetup-details.html', {'selected_meetup': selected_meetup})
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(request, 'meetups/meetup-details.html', {'selected_meetup': selected_meetup, 'meetup_found': True})
+    except Exception as exc:
+        return render(request, 'meetups/meetup-details.html', {'meetup_found': False})
