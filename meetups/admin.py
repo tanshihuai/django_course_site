453ecf8ae829563a44b9c9827a125a50740e993e
shihuai <tanshihuai@gmail.com>
2022-08-18 02:16:40 +0800

    models done

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Meetup
+from .models import Meetup, Location, Participant
 
 # Register your models here.
 
 admin.site.register(Meetup)
+admin.site.register(Location)
+admin.site.register(Participant)
--- meetups/models.py
+++ meetups/models.py
@@ -2,8 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.name}, {self.location}'
+
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    location = models.TextField()
     image = models.ImageField(upload_to='images')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(Participant, blank=True, null=True)
+    organizer_email = models.EmailField()
+    date = models.DateField()
+
+    
